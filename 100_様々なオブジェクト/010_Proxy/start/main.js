7957aa50d1f5db4af5b1f92623748f073912a32e
youliangdao <youliangdaojing5@gmail.com>
2022-07-27 17:41:52 +0900

    chapter10-1

--- 100_様々なオブジェクト/010_Proxy/start/main.js
+++ 100_様々なオブジェクト/010_Proxy/start/main.js
@@ -1,1 +1,25 @@
 const targetObj = { a: 0 };
+
+const handler = {
+  set: function (target, prop, value, receiver) {
+    console.log(`[set]: ${prop}`)
+    // target[prop] = value;
+    throw new Error('cannot add prop.');
+  },
+  get: function (target, prop, receiver) {
+    console.log(receiver)
+    if (target.hasOwnProperty(prop)) {
+      return target[prop];
+    } else {
+      return '-1';
+    }
+  },
+  deleteProperty: function (target, prop) {
+    console.log(`[delete]: ${prop}`)
+    delete target[prop]
+  }
+}
+const proxy = new Proxy(targetObj, handler);
+console.log(proxy.b);
+// delete proxy.a;
+// console.log(proxy.a)
--- 100_様々なオブジェクト/020_Reflect/start/main.js
+++ 100_様々なオブジェクト/020_Reflect/start/main.js
@@ -1,24 +1,34 @@
-class C {
-  constructor(a, b) {
-    this.a = a;
-    this.b = b;
-  }
-}
-
-
-// const bob = {
-//   name: 'Bob',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
+// class C {
+//   constructor(a, b) {
+//     this.a = a;
+//     this.b = b;
 //   }
 // }
 
-// const tom = {
-//   name: 'Tom',
-//   _hello: function () {
-//     console.log(`hello ${this.name}`);
-//   },
-//   get hello() {
-//     return this._hello();
-//   },
-// }
+// const obj1 = new C(1, 2);
+// console.log(obj1);
+// const obj2 = Reflect.construct(C, [1, 2]);
+// console.log(obj2);
+
+// console.log("c" in obj1);
+// console.log (Reflect.has(obj1, "c"))
+
+const bob = {
+  name: 'Bob',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  }
+}
+
+const tom = {
+  name: 'Tom',
+  _hello: function () {
+    console.log(`hello ${this.name}`);
+  },
+  get hello() {
+    console.log(this)
+    return this._hello();
+  },
+}
+tom.hello;
+Reflect.get(tom, "hello", bob);
